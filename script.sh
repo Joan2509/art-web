5eb99a2b7803eef9534932a9d31690dcc7f15f37
rogwel <anxielworld@gmail.com>
2024-08-14 19:01:33 +0300

    [Update]: Update the  dockerize project

--- script.sh
+++ script.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# build the container
-docker build -t ascii-art-web:art-web .
+# Build the Docker image
+docker build -t ascii-art-web:latest .
 
-# Run the container application
+#hgmyutytuy
+docker stop $(docker ps -q)
 
-docker run -it --name myapp-container -p 8080:8080 myapp:latest
+# Remove any stopped containers
+docker container prune -f
 
+# Remove any dangling images (images not associated with a container)
+docker image prune -f
 
+# Run the container 
+echo "Running the Docker container at http://localhost:8000"
+docker run -d -p 8000:8000 --name dockerize-it ascii-art-web:latest
 
